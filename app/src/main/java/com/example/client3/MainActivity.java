d06d86d691d97e6314e39fe0c6ca899117e0716c
Slava <slavaguk2000@gmail.com>
2019-04-19 21:04:44 +0300

    create beautiful gui

--- app/src/main/java/com/example/client3/MainActivity.java
+++ app/src/main/java/com/example/client3/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.client3;
 
+
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
@@ -20,13 +23,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView informationText;
+    private TextView titleText;
+    private ProgressBar sendProgressBar;
+    private Button cancelButton;
 
     private EditText ipAddressEditText;
     private EditText messageEditText;
@@ -38,12 +51,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         context = this;
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ipAddressEditText = (EditText)findViewById(R.id.editTextIP);
         messageEditText = (EditText)findViewById(R.id.editTextMessage);
+        informationText = (TextView)findViewById(R.id.informationText);
+        titleText = (TextView)findViewById(R.id.titleText);
+        sendProgressBar = (ProgressBar)findViewById(R.id.sendProgressBar);
+        cancelButton = (Button)findViewById(R.id.cancelButton);
         stringsList = new ArrayList<String>();
-        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this) && checkPermissionWRITE_EXTERNAL_STORAGE(this)) {
             getImagePathes(this);
             permission = true;
         }
@@ -54,13 +72,13 @@ public class MainActivity extends AppCompatActivity {
         AsyncTask<Void, Void, Void> getLocalIp = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... voids) {
-                    ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-                    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-                    WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
 
-                    WifiInfo connectionInfo = wm.getConnectionInfo();
-                    int ipAddress = connectionInfo.getIpAddress();
-                    String myIp = Formatter.formatIpAddress(ipAddress);
+                WifiInfo connectionInfo = wm.getConnectionInfo();
+                int ipAddress = connectionInfo.getIpAddress();
+                String myIp = Formatter.formatIpAddress(ipAddress);
                 final String finalMyIp = new String(myIp);
                 runOnUiThread(new Runnable() {
                     @Override
@@ -92,13 +110,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+    public static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 234;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // do your stuff
+                } else {
+                    grantResults[0] = PackageManager.PERMISSION_GRANTED;
+                    Toast.makeText(this, "GET_ACCOUNTS Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 } else {
                     grantResults[0] = PackageManager.PERMISSION_GRANTED;
                     Toast.makeText(this, "GET_ACCOUNTS Denied",
@@ -140,6 +166,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public boolean checkPermissionWRITE_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
     public void showDialog(final String msg, final Context context,
                            final String permission) {
         AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
@@ -159,15 +214,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void send_images(View v){
+        LoaderImages(v, true);
+    }
+    public void download_images(View v){
+        LoaderImages(v, false);
+    }
+    public void LoaderImages(View v, boolean isSend){
         try{
-            Button send_button = (Button)v;
+            closeKeyboard(v);
             String ipAddress = ipAddressEditText.getText().toString();
             String name = messageEditText.getText().toString().replace('@', 'a');
             if (!permission) return;
-            SenderImages send = new SenderImages(imagesPaths, name, send_button, this);
+            ArrayList<Button> buttons = new ArrayList<>();
+            buttons.add((Button)findViewById(R.id.sendButton));
+            buttons.add((Button)findViewById(R.id.downloadButton));
+            SenderImages send = new SenderImages(imagesPaths, name, buttons, isSend, this,
+                    informationText, titleText, sendProgressBar, cancelButton, messageEditText);
             send.execute(ipAddress);
         }catch (Exception e){}
     }
 
+    public void closeKeyboard(View v){
+        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
 
 }
--- app/src/main/java/com/example/client3/SenderImages.java
+++ app/src/main/java/com/example/client3/SenderImages.java
@@ -5,78 +5,185 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import android.provider.Settings.Secure;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SenderImages extends AsyncTask<String, Void, String> {
-
+    DataOutputStream writer;
+    DataInputStream reader;
     List<String> imagesPathes;
     Socket socket;
+    boolean isSend;
     String personName;
-    Button send_button;
+    final ArrayList<Button> buttons;//0-send, 1-download
     Context context;
-    SenderImages(List<String> imagesPathes, String personName, Button send_button, Context context){
+    private static TextView informationText;
+    private static TextView titleText;
+    private static ProgressBar sendProgressBar;
+    private static Button cancelButton;
+    private static View nameText;
+    private static boolean isWorking = false;
+    SenderImages(List<String> imagesPathes, String personName, ArrayList<Button> buttons, boolean isSend, Context context,
+                 TextView informationText, TextView titleText, ProgressBar sendProgressBar, Button cancelButton, View nameText){
         this.imagesPathes = imagesPathes;
         this.personName = personName;
-        this.send_button = send_button;
         this.context = context;
+        this.buttons = buttons;
+        this.isSend = isSend;
+        this.informationText = informationText;
+        this.titleText = titleText;
+        this.sendProgressBar = sendProgressBar;
+        this.cancelButton = cancelButton;
+        this.nameText = nameText;
     }
-    void changeButtonState(Button btn, boolean state){
+    private void setButtonsEnable(boolean state){
         final boolean finalState = state;
-        final Button finalBtn = btn;
         Handler handler = new Handler(Looper.getMainLooper());
         handler.post(new Runnable() {
             public void run() {
-                finalBtn.setEnabled(finalState);
+                for (Button btn: buttons) {
+                    btn.setEnabled(finalState);
+                }
             }
         });
     }
+    private void setInformationVisible(boolean state){
+        final int finalState = state? View.VISIBLE:View.GONE;
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                informationText.setVisibility(finalState);
+                titleText.setVisibility(finalState);
+                sendProgressBar.setVisibility(finalState);
+                cancelButton.setVisibility(finalState);
+            }
+        });
+    }
+    private void setProgress(final int progress, final String information){
+        final int finalProgress = progress;
+        final String finalInformation;
+        if (progress > 0) finalInformation = information + progress + "%";
+        else finalInformation = information;
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                if (progress > 0) sendProgressBar.setProgress(finalProgress);
+                informationText.setText(finalInformation);
+            }
+        });
+    }
+    private void changeButton(boolean state){
+        final String buttonText;
+        final View.OnClickListener onClickListener;
+        final String finalTitleText;
+        if (state){
+            buttonText = "OK";
+            finalTitleText = "Copying files was comlited";
+            onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setInformationVisible(false);
+                }
+            };
+        }else{
+            buttonText = "CANCEL";
+            isWorking = true;
+            finalTitleText = "Copying files...";
+            onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    isWorking = false;
+                    changeButton(true);
+                }
+            };
+        }
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+                setInformationVisible(true);
+                cancelButton.setText(buttonText);
+                cancelButton.setOnClickListener(onClickListener);
+                titleText.setText(finalTitleText);
+            }
+        });
+    }
+    private Vector<String> getImagesFromPreviousCopy() throws IOException{
+        Vector<String> imagesFromPreviousCopy = new Vector<>();
+        int imagesFromPreviousCopyCount = reader.readInt();
+        for (int i = 0; i < imagesFromPreviousCopyCount; i++) {
+            imagesFromPreviousCopy.add(reader.readUTF().replace("\\", "/"));
+        }
+        return imagesFromPreviousCopy;
+    }
     @Override
     protected String doInBackground(String... params){
-        String output = "";
-        try {
-            socket = new Socket(params[0], 2154);
-            changeButtonState(send_button, false);
+         try {
+            socket = new Socket(params[0], 4242);
+            setButtonsEnable(false);
 
             String myDeviceName = Build.MODEL + "_" + Secure.getString(context.getContentResolver(),Secure.ANDROID_ID);
-            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
-            DataInputStream reader = new DataInputStream(socket.getInputStream());
+            writer = new DataOutputStream(socket.getOutputStream());
+            reader = new DataInputStream(socket.getInputStream());
 
-            int readCount;
-            File currentImage;
-            FileInputStream fileReader;
-            int imagesFromPreviusCopyCount;
-            Vector<String> imagesFromPreviusCopy = new Vector<>();
-            int countOfImage = imagesPathes.size();
-            if (countOfImage > 20) countOfImage = 20;//////////////!!!!!!!!!!!!!!!!!!!!!!!!for debug
-            ///// Logic of write
-
+            //Initialising
             writer.writeUTF(myDeviceName);
             writer.writeUTF(personName);
-            writer.writeInt(countOfImage);
+            writer.writeBoolean(isSend);
             writer.flush();
-            imagesFromPreviusCopyCount = reader.readInt();
-            for (int i = 0; i < imagesFromPreviusCopyCount; i++) {
-                imagesFromPreviusCopy.add(reader.readUTF().replace("\\", "/"));
-            }
+            changeButton(false);
+            if (isSend) send_images();
+            else download_images();
+            changeButton(true);
+            socket.close();
+        }catch(UnknownHostException ex){
+            System.out.println("UnknownHostException");
+        }catch(IOException ex){
+            System.out.println("IOException");
+        }catch (Exception ex){
+            System.out.println("Exception");
+        }
+        setButtonsEnable(true);
+        return null;
+    }
 
-            for(int i = 0; i < countOfImage; i++){
+    private void send_images()throws IOException {
+        String information = "Sending files: ";
+        setProgress(0, information);
+        Vector<String> imagesFromPreviousCopy = getImagesFromPreviousCopy();
+        FileInputStream fileReader;
+        File currentImage;
+        int readCount;
+        ///// Logic of write
+        int countOfImage = imagesPathes.size();
+        if (countOfImage > 100) countOfImage = 100; /////////////for debugging
+        writer.writeInt(countOfImage);
+        String failedInformation = "Copying was failed";
+        try {
+            long endFileLengty = 0;
+            for (int i = 0; i < countOfImage; i++) {
+                setProgress(100 * i / countOfImage, information);
                 String currentImagePath = imagesPathes.get(i);
-                if (!imagesFromPreviusCopy.contains(currentImagePath)) {
+                if (!imagesFromPreviousCopy.contains(currentImagePath)) {
                     currentImage = new File(currentImagePath);
-
                     long fileLength = currentImage.length();
-
                     writer.writeLong(fileLength);
                     writer.writeUTF(currentImagePath);
                     fileReader = new FileInputStream(currentImage);
@@ -86,23 +193,92 @@ public class SenderImages extends AsyncTask<String, Void, String> {
                     }
                     writer.flush();
                     fileReader.close();
+                    if (!isWorking) {
+                        endFileLengty = 1;
+                        break;
+                    }
+                } else {
+                    writer.writeLong(1);
+                    writer.writeUTF("exist");
                 }
             }
-            writer.writeLong(0);
+            if (endFileLengty == 1) setProgress(-1, failedInformation);
+            else setProgress(100, information);
+            writer.writeLong(endFileLengty);
             writer.writeUTF("null");
-            wait(5000);
-            socket.close();
-            changeButtonState(send_button, true);
-        }
-        catch(UnknownHostException ex){
-            System.out.println("UnknownHostException");
+            try {
+                wait(5000);
+            } catch (Exception ex) {}
         }catch(IOException ex){
-            System.out.println("IOException");
+            setProgress(-1, failedInformation);
         }
-        catch (Exception ex){
-            System.out.println("Exception");
-        }
+    }
 
-        return null;
+    private void download_images() throws IOException{
+        String information = "Downloading files: ";
+        setProgress(0, information);
+        boolean existSaving = reader.readBoolean();
+        if (existSaving) {
+            Vector<String> imagesFromCurrentCopy = getImagesFromPreviousCopy();
+            FileOutputStream fileWriter;
+            Vector<Integer> numbersOfNeeds = new Vector<>();
+            int imagesFromCurrentCopyCount = imagesFromCurrentCopy.size();
+            for (int i = 0; i < imagesFromCurrentCopyCount; i++) {
+                if (!imagesPathes.contains(imagesFromCurrentCopy.get(i))) {
+                    numbersOfNeeds.add(i);
+                }
+            }
+            int numberOfNeedsCount = numbersOfNeeds.size();
+            writer.writeInt(numberOfNeedsCount);
+            for (int number : numbersOfNeeds) {
+                writer.writeInt(number);
+            }
+            writer.flush();
+            for (int i = 0; i < numberOfNeedsCount; i++) {
+                setProgress(100*i/numberOfNeedsCount, information);
+                long fileLength = reader.readLong();
+                String filePath = reader.readUTF();
+                byte[] readBuffer = new byte[64 * 1024];
+                File image = new File(filePath);
+                image.getParentFile().mkdirs();
+                fileWriter = new FileOutputStream(filePath);
+                int count, total = 0;
+                while ((count = reader.read(readBuffer, 0, (int) Math.min(readBuffer.length, fileLength - total))) != -1) {
+                    total += count;
+                    fileWriter.write(readBuffer, 0, count);
+
+                    if (total == fileLength) {
+                        break;
+                    }
+                }
+                fileWriter.flush();
+                fileWriter.close();
+                if(!isWorking) break;
+            }
+            setProgress(100, information);
+        }else{
+            PopupMenu popupMenu = new PopupMenu(context, nameText);
+            Vector<String> existNames= new Vector<>();
+            int namesSize = reader.readInt();//количество сохранений
+            for (int i = 0; i < namesSize; i++) {
+                String currentName = reader.readUTF();
+                existNames.add(currentName);
+                popupMenu.getMenu().add(currentName);
+            }//передача сохранений
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(final MenuItem item) {
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        public void run() {
+                            EditText nameEditText = (EditText)nameText;
+                            nameEditText.setText(item.getTitle().toString());
+                        }
+                    });
+                    return false;
+                }
+            });
+            popupMenu.show();
+        }
     }
 }
